6a00bad819f80fe193435b226cf30d739eb64f19
j2doll <j2doll@gmail.com>
2019-07-17 13:15:26 +0900

    update test code

--- bind/main.cpp
+++ bind/main.cpp
@@ -31,11 +31,11 @@ int main(int argc, char *argv[])
 
     //}} [0]
 
-    // set context
+    // [1] set context
     CppValue* cppValue1 = new CppValue( &app );
     engine.rootContext()->setContextProperty( "cppValue1", cppValue1 );
 
-    // [1] create timer object (for testing)
+    // [2] create timer object (for testing)
     QMLTimer* qmlTimer = new QMLTimer( &app );
 
     int rootObjSize = engine.rootObjects().size();
@@ -45,22 +45,23 @@ int main(int argc, char *argv[])
         if ( obj == nullptr )
             continue;
 
-        // [2] find the root object that has 'objectName'. 'objectName' is 'mainWindow'.
+        // [3] find the root object that has 'objectName'. 'objectName' is 'mainWindow'.
         QVariant val = obj->property( "objectName" );
         if ( false == val.isNull() &&
              val.toString() == QString("mainWindow") )
         {
-            // [2-1] success to find 'mainWindow'
+            // [4] success to find 'mainWindow'
             QObject* mainWindowObj = obj;
             mainWindowObj->setProperty( "title", QVariant(QString("HELLO WORLD")) ); // change 'title' of 'mainWindow'
 
-            // [3] find child object. which objectName is 'mainText'.
+            // [5] find child object. which objectName is 'mainText'.
             QObject *textObj = mainWindowObj->findChild<QObject*>( "mainText" );
             if ( textObj != nullptr )
             {
                 qmlTimer->objText = textObj;
 
-                QVariant valX = textObj->property( "x" ); // get 'x' from 'mainText'
+                // [6] get 'x' from 'mainText'
+                QVariant valX = textObj->property( "x" );
                 qDebug() << "x : " << valX;
             }
         }
